7c3e1c6601dd428a83aae7b0f62d9ee9ead8cec5
G Johansson <goran.johansson@shiftit.se>
2021-08-07 18:59:39 +0200

    Addition on exceptions

--- yalesmartalarmclient/exceptions.py
+++ yalesmartalarmclient/exceptions.py
@@ -10,3 +10,17 @@ class AuthenticationError(Exception):
     def __init__(self, *args: Any) -> None:
         """Initialize the exception."""
         Exception.__init__(self, *args)
+
+class HTTPError(Exception):
+    """Exception to indicate an issue with the connection against the Yale Smart API."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+class ConnectionError(Exception):
+    """Exception to indicate an issue with the general connection."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
